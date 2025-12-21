f7105d980549c29863aaa5be8fc41868ed1e91c3
Ranbir <ranbir.webdev@gmail.com>
2025-12-21 11:08:28 +0600

    add some changes of firebase.config.js

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,21 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDBJnGIztVnwDYZ0dvX2PoscT97u88pUzo",
-  authDomain: "home-nest-e972c.firebaseapp.com",
-  projectId: "home-nest-e972c",
-  storageBucket: "home-nest-e972c.firebasestorage.app",
-  messagingSenderId: "34726747320",
-  appId: "1:34726747320:web:cdf86beb7cc2958ba2684d",
-  measurementId: "G-PXWX8EYXXF"
+  apiKey: "AIzaSyCAtbixJKsGr0wzDvuMyF_6bhXj5CQVqJE",
+  authDomain: "home-nest-project-bc7aa.firebaseapp.com",
+  projectId: "home-nest-project-bc7aa",
+  storageBucket: "home-nest-project-bc7aa.appspot.com",
+  messagingSenderId: "29081505814",
+  appId: "1:29081505814:web:3882a94175c502dd24e537"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
